2189d126d5932665bb6a83a47c0b1d7d24304eab
Alexander Borzunov <hxrussia@gmail.com>
2015-11-26 21:14:10 +0500

    Ninject

--- TagCloudGenerator/TagCloudGenerator/Program.cs
+++ TagCloudGenerator/TagCloudGenerator/Program.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using Ninject;
 using TagCloudGenerator.CloudGenerators;
 using TagCloudGenerator.CloudRenderers;
 using TagCloudGenerator.GrammarInfo;
 using TagCloudGenerator.WordsFilters;
+using TagCloudGenerator.WordsSources;
 using TagCloudGenerator.WordsSources.TextSources;
 
 namespace TagCloudGenerator
@@ -14,24 +16,41 @@ namespace TagCloudGenerator
     {
         static void Main()
         {
-            var cloudProcessor = new CloudProcessor(
-                new TextDocumentReader("WarAndPeace_Part1.txt"),
-                new MystemGrammarInfoParser(),
-                new IWordsFilter[]
+            var container = new StandardKernel();
+
+            container.Bind<IWordsSource>().To<TextDocumentReader>()
+                .WithConstructorArgument("WarAndPeace_Part1.txt");
+
+            container.Bind<IGrammarInfoParser>().To<MystemGrammarInfoParser>();
+
+            container.Bind<PartsOfSpeechFilter>().ToSelf()
+                .WithConstructorArgument(new HashSet<PartOfSpeech>
                 {
-                    new PartsOfSpeechFilter(new HashSet<PartOfSpeech>
-                    {
-                        PartOfSpeech.Adjective,
-                        PartOfSpeech.Noun,
-                    }),
-                    new GrammarFormsJoiner(),
-                    new MostCommonWordsFilter(20),
-                },
-                new CenteredCloudGenerator(Color.White, Color.Green, "Times New Roman", new Size(350, 350),
-                    new Random()), 
-                new BitmapRenderer("output.png", ImageFormat.Png)
-            );
-            cloudProcessor.Process();
+                    PartOfSpeech.Adjective,
+                    PartOfSpeech.Noun,
+                });
+            container.Bind<MostCommonWordsFilter>().ToSelf()
+                .WithConstructorArgument("count", 20);
+
+            container.Bind<ICloudGenerator>().To<CenteredCloudGenerator>()
+                .WithConstructorArgument("backgroundColor", Color.White)
+                .WithConstructorArgument("textColor", Color.Green)
+                .WithConstructorArgument("fontFamilyName", "Times New Roman")
+                .WithConstructorArgument("size", new Size(350, 350));
+
+            container.Bind<ICloudRenderer>().To<BitmapRenderer>()
+                .WithConstructorArgument("filename", "output.png")
+                .WithConstructorArgument("format", ImageFormat.Png);
+
+            container.Bind<CloudProcessor>().ToSelf()
+                .WithConstructorArgument("wordsFilters", new IWordsFilter[]
+                {
+                    container.Get<PartsOfSpeechFilter>(),
+                    container.Get<GrammarFormsJoiner>(),
+                    container.Get<MostCommonWordsFilter>(),
+                });
+
+            container.Get<CloudProcessor>().Process();
         }
     }
 }
